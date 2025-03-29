6aa1af667a7ef0e03a2a1c5d6b031fa9c39ee0a6
Spacedestructor <spacedestructor@gmail.com>
2025-03-29 03:35:23 +0100

    Delete scripts/BlueprintHandlers.lua
    Deleting unused file.

--- scripts/BlueprintHandlers.lua
+++
@@ -1,9 +0,0 @@
-local function DecodeBlueprint(Blueprintstring)
-	return helpers.decode_string(Blueprintstring)
-end
-
-local function EncodeBlueprint(Table)
-	return helpers.encode_string(Table)
-end
-
-return DecodeBlueprint, EncodeBlueprint
